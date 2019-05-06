955604976eb848e0b638d1455f9b392c429ed639
Mildred <mi.mitel@hotmail.com>
2019-05-05 20:41:12 -0500

    student table

---
+++ database/migrations/2019_05_06_011953_create_estudiante_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstudianteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('Estudiante', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('idMatricula')->unique();
+        $table->string('nombre_completo');
+        $table->string('celular');
+        $table->integer('semestre_actual');
+        $table->string('id_carrera');
+        $table->boolean('asignable_sn');
+        $table->boolean('estatus_assignable_sn');
+        $table->integer('tipo_beca');
+        $table->string('contrasena');
+        $table->string('email');
+        $table->rememberToken();
+        $table->timestamps();
+        $table->softDeletes();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('Estudiante');
+    }
+}
