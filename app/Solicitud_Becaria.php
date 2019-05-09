646aabadae7e31af3e637b3518ebb0eba2564526
JaimeMontemayor <jaimemontemayor97@gmail.com>
2019-05-09 18:35:22 -0500

    constantes

--- app/Solicitud_Becaria.php
+++ app/Solicitud_Becaria.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Solicitud_Becaria extends Model
 {
     protected $table = 'solicitud_becaria';
-    
+    const INVIERNO = 1;
+    const FEB_JUN  = 2;
+    const VERANO   = 3;
+    const AGO_DIC  = 4;
     public function estudiante() {
         return $this->belongsTo('App\Estudiante', 'id_estudiante');
     }
