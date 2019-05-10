79848c1f14dd475e62818f1e91fbf34f5dc03b76
Arely Aceves <arelyac01@gmail.com>
2019-05-10 15:23:12 -0500

    Modal Colaboradores

--- resources/assets/js/views/homeColaboradores.jsx
+++ resources/assets/js/views/homeColaboradores.jsx
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import TextField from '@material-ui/core/TextField';
+import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import defaultTheme from '../theme';
 
-const Login = ({classes}) => {
-  const [inputName, setInputName] = React.useState('');
-  const [inputPassword, setInputPassword] = React.useState('');
+const homeColaboradores = ({classes}) => {
+  const [open, setOpen]  = React.useState(false);
 
-  const onChangeName = (event) => {
-    setInputName(event.target.value);
-  }
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-  const onChangePassword = (event) => {
-    setInputPassword(event.target.value);
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
   
   return (
     <MuiThemeProvider theme={defaultTheme}>
+			<Button onClick={handleOpen}>Open Modal</Button>
+			<Modal
+				open={open}
+				onClose={handleClose}
+			>
+				<div className={`container ${classes.containerWidth}`}>
+					<div className="card">
+						<div className="row">
+							<div className={`col s12`}>
+								<div className="row">
+									<div className="col s12">
+										<label className={classes.labelHeader}>
+												Perfil.
+										</label>
+										<br></br>
+										<br></br>
+										<div className="col s6">
+											<div className="row">
+												<label className={classes.labelText}>
+													Nombre
+												</label>
+											</div>
+											<div className="row">
+												<label className={classes.labelText}>
+													Departamento
+												</label>
+											</div>
+											<div className="row">
+												<label className={classes.labelText}>
+													Mail
+												</label>
+											</div>
+										</div>
+										<div className="col s6">
+											<div className="row">
+												<label className={classes.labelText}>
+													Oficina
+												</label>
+											</div>
+											<div className="row">
+												<label className={classes.labelText}>
+													Telefono
+												</label>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Modal>
     </MuiThemeProvider>
   );
 }
@@ -41,6 +93,9 @@ const styles = theme => ({
   labelText: {
     fontSize: '14px',
     color: '#000',
+	},
+	containerWidth: {
+    maxWidth: 450,
   },
 
   [`@media (max-width: ${maxWidth}px)`]: {
@@ -48,8 +103,8 @@ const styles = theme => ({
   }
 });
 
-const _Login = withStyles(styles)(Login);
+const _homeColaboradores = withStyles(styles)(homeColaboradores);
 
-if (document.getElementById('login-modal')) {
-  ReactDOM.render(<_Login/>, document.getElementById('login-modal'));
+if (document.getElementById('homeColaborador')) {
+  ReactDOM.render(<_homeColaboradores/>, document.getElementById('homeColaborador'));
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,4 +10,5 @@ const { mix } = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.react('resources/assets/js/views/login.jsx', 'public/js/views');
+mix.react('resources/assets/js/views/login.jsx', 'public/js/views')
+    .react('resources/assets/js/views/homeColaboradores.jsx', 'public/js/views');
