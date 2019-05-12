40ca6dfaa904a6fc3785c6087553f8cdb603817c
Mildred <mi.mitel@hotmail.com>
2019-05-12 13:43:24 -0500

    users table

--- app/User.php
+++ app/User.php
@@ -8,21 +8,30 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  protected $table = 'estudiante';
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'username', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token',
+  ];
+
+  const ADMINISTRADOR = 'App\Admin';
+  const ESTUDIANTE    = 'App\Estudiante';
+  const COLABORADOR   = 'App\Colaborador';
+  
+  public function payable() {
+    return $this->morphTo();
+  }
 }
